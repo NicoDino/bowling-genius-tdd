88aed811ae8aa038759e55eaade18841a505b7c7
ndinolfo <nicodinolfo@gmail.com>
2019-04-18 18:57:54 -0300

    agrega test all ones

--- test.js
+++ test.js
@@ -4,8 +4,16 @@ let Game = require('./bowling');
 describe("Bowling", function () {
 
   let game = new Game();
+
   it("gutter game", function () {
     game.getScore().should.equal(0);
+  });
+
+  it("all ones", function () {
+    for (i = 0; i < 20; i++) {
+      game.shoot(1);
+    }
+    game.getScore().should.equal(20);
   });
 
 });
