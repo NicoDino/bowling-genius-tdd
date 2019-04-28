2ee3ad73b47f1f4812611716babccf3fef6ff366
Agus <agusgc_99@hotmail.com>
2019-04-28 16:50:35 -0300

    one spare y gutter+10=spare pasando

--- bowling.js
+++ bowling.js
@@ -1,8 +1,35 @@
 let Game = function () {
     let rolls = [];
+    var firtsShoot = false;
+    var isSpare = false;
+    var isStrike = false;
+    var score;
+    var lastPins;
 
     this.roll = function (pinsKnocked) {
-        rolls.push(pinsKnocked);
+        firtsShoot=!firtsShoot;
+        lastPins=0;
+        score=pinsKnocked;
+        if(!firtsShoot){
+            lastPins=rolls[rolls.length-1];
+        }
+        
+        if(isStrike){
+            score = addStrikeBonus();
+        }
+        if(isSpare){
+            score = score+pinsKnocked;
+            isSpare=false;
+        }
+        if(pinsKnocked==10 && firtsShoot){
+            isStrike=true;
+        }
+        if(pinsKnocked+lastPins==10 && !firtsShoot) {
+            isSpare=true;
+        }
+
+
+        rolls.push(score);
     }
 
     this.getScore = function () {
