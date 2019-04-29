4ebb2fe533433f371456f9a2520ab73db6c4c925
ndinolfo <nicodinolfo@gmail.com>
2019-04-29 20:08:01 -0300

    chequeo strike y spare en funcion

--- bowling.js
+++ bowling.js
@@ -12,10 +12,10 @@ let Game = function () {
 
         // calcula la suma de cada "frame" (par de tiros)
         for (let frame = 0; frame < 10; frame++) {
-            if (rolls[rollNumber] === 10) {
+            if (esStrike(rolls, rollNumber)) {
                 totalScore += 10 + rolls[rollNumber + 1] + rolls[rollNumber + 2];
                 rollNumber++; // Solo cuando es strike se realiza un solo tiro
-            } else if (rolls[rollNumber] + rolls[rollNumber + 1] === 10) {
+            } else if (esSpare(rolls, rollNumber)) {
                 totalScore += 10 + rolls[rollNumber + 2];
                 rollNumber += 2;
             } else {
@@ -28,4 +28,12 @@ let Game = function () {
 }
 
 module.exports = Game;
+
+function esSpare(rolls, rollNumber) {
+    return rolls[rollNumber] + rolls[rollNumber + 1] === 10;
+}
+
+function esStrike(rolls, rollNumber) {
+    return rolls[rollNumber] === 10;
+}
 
